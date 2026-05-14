34521e6835f5c392f866691b932e83986972b69f
Sergio Coelho <sergiocoelho.trp@gmail.com>
2026-05-14 15:46:57 +0100

    add simple program memory draw

---
+++ Memory/SimpleExample.cs
@@ -0,0 +1,12 @@
+public class Program
+{
+    public static void Main()
+    {
+        // stored in the stack
+        int x = 42;
+        // stored in the heap
+        string name = "Sebastian";
+        // stored in the heap
+        int[] numbers = [1, 2, 3, 4, 5];
+    }
+}
